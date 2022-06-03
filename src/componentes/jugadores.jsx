dd81bab069b29734ca966634b5cd3e43ab98a29e
Brian_Zapata_Marin <brianezm@gmail.com>
2022-06-03 01:45:56 -0300

    botones agregados

--- src/componentes/jugadores.jsx
+++ src/componentes/jugadores.jsx
@@ -4,23 +4,37 @@ import "../componentes/jugadores.css"
 export const JugadoresSorteo = ({initialPlayers}) => {
 
   const [players, setPlayers] = useState([]);
+  const [visible, setVisible] = useState(true);
+  const [reset, setReset] = useState(false)
 
   const mezclar = () => {
 
     setPlayers(initialPlayers.sort(()=> Math.round(Math.random())-1));
     console.log(players);
+    setVisible(false);
+    setReset(true);
+
+  }
+
+  const volver = () => {
+      setReset(false);
+      setVisible(true)
   }
 
     return(
         <>
+            {visible ? 
             <button onClick={mezclar} className="boton">Sortear</button>
-            {players.map((player)=>{
+            :
+            players.map((player)=>{
+
                 return(
                     <div key={player.id}>
                         <p>Jugador: {player.jugador}</p>
                     </div>
                     )
                 })}
+            {reset && <button onClick={volver} className="boton">Reiniciar</button>}
         </>
     )
 }
