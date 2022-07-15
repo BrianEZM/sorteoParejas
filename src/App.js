e21914dc23b39fc019668f74e61b208660c65e46
brianezm <brianezm@gmail.com>
2022-07-14 21:18:22 -0300

    modificado para sortear enfrentamientos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { JugadoresSorteo } from "../src/componentes/jugadores"
 
 const initialPlayers = [
-  {jugador: "Emma", id: 1},
-  {jugador: "Brian", id: 2},
-  {jugador: "Roswel", id: 3},
-  {jugador: "Neger", id: 4},
-  {jugador: "Beto", id: 5},
-  {jugador: "Gaby", id: 6}
+  {jugador: "Emma y Gaby", id: 1},
+  {jugador: "Brian y Roswel", id: 2},
+  {jugador: "Beto y Neger", id: 3},
+  {jugador: "Eze y Gonza", id: 4}
+  // {jugador: "Beto", id: 5},
+  // {jugador: "Gaby", id: 6}
 ];
 
 function App() {
--- src/componentes/jugadores.jsx
+++ src/componentes/jugadores.jsx
@@ -12,11 +12,25 @@ export const JugadoresSorteo = ({initialPlayers}) => {
     setPlayers(initialPlayers.sort(()=> Math.round(Math.random())-1));
     setVisible(false);
     setReset(true);
+ 
   }
 
     let pareja1 = [players[0], players[1]];
     let pareja2 = [players[2], players[3]];
-    let pareja3 = [players[4], players[5]];
+    // let pareja3 = [players[4], players[5]];
+
+    
+    // PROBANDO REFACTOREAR CON REDUCE
+    // const couples = players.reduce((acc, cur) => {
+    //     return acc = {id: players.id,
+    //                   couple: players.jugador}
+    // }, {});
+
+    // console.log(players.id);
+
+    // console.log(`LOG DE REDUCE: ${couples}`);
+    // console.log(couples);
+
 
   const volver = () => {
       setReset(false);
@@ -26,31 +40,33 @@ export const JugadoresSorteo = ({initialPlayers}) => {
 //   RENDERIZADO
     return(
         <>
+            <h3>Cancha 1</h3>
             {visible ? 
             <button onClick={mezclar} className="boton">Sortear</button>
             :
             pareja1.map((player)=>{
                 return(
                     <div className='dupla' key={player.id}>
-                        <p>Pareja 1: {player.jugador}</p>
+                        <p>{player.jugador}</p>
                     </div>
                     )
                 })
                 }
+            <h3>Cancha 2</h3>
             {!visible && pareja2.map((player)=>{
                 return(
                     <div className='dupla' key={player.id}>
-                        <p>Pareja 2: {player.jugador}</p>
+                        <p>{player.jugador}</p>
                     </div>
                     )
                 })}
-            {!visible && pareja3.map((player)=>{
+            {/* {!visible && pareja3.map((player)=>{
                 return(
                     <div className='dupla' key={player.id}>
                         <p>Pareja 3: {player.jugador}</p>
                     </div>
                     )
-                })}
+                })} */}
             {reset && <button onClick={volver} className="boton">Reiniciar</button>}
         </>
     )
